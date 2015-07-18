e2073097c93c0e5b8cb7e6900053a3521e1642c9
张胜东 <435878393@qq.com>
2015-07-18 19:42:59 +0800

    不用管此时的vs找不到_ConnectionPtr

--- 网络聊天室客户端/网络聊天室客户端Dlg.h
+++ 网络聊天室客户端/网络聊天室客户端Dlg.h
@@ -28,4 +28,8 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+
+
+public:
+	_ConnectionPtr m_ClientDB;				//连接ADO数据库,不用管此时的vs找不到_ConnectionPtr
 };
