b875757573405e0265dce860246350ecd0b33219
张胜东 <435878393@qq.com>
2015-07-18 15:35:04 +0800

    客户端初始

---
+++ 网络聊天室客户端/网络聊天室客户端Dlg.h
@@ -0,0 +1,31 @@
+
+// 网络聊天室客户端Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// C网络聊天室客户端Dlg 对话框
+class C网络聊天室客户端Dlg : public CDialogEx
+{
+// 构造
+public:
+	C网络聊天室客户端Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_MY_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
